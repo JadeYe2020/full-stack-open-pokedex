ce3311c61315393a82d13a6da6fe5f63815a1aab
Jade Ye <w0430394@nscc.ca>
2022-10-02 16:03:19 -0300

    Finish 11.12: custom health checks with a shell script.

--- health_check.sh
+++ health_check.sh
@@ -2,4 +2,14 @@
 
 echo "Hello from shell script"
 
+VAR="$(curl https://fullstackopen-cicd.fly.dev/health)"
+
+echo "$VAR is returned"
+
+if [ "$VAR" == "ok" ]; then
+  exit 0
+else
+  exit 1
+fi
+
 exit 0 # exit status 0 means that the script "succeeds"
